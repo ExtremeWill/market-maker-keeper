7170db10d425ae942f34c2612e61741d452efc1e
张蔚杰 <zhangweijie@shengpay.com>
2020-02-28 17:47:09 +0800

    c

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 set -e
 
 rm -rf _virtualenv
-python3 -m virtualenv _virtualenv
+virtualenv _virtualenv
 source _virtualenv/bin/activate
 
 # The advantage of using this method, in contrary to just calling `pip install -r requirements.txt` several times,
